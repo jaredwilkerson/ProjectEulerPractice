7adfee47bfb94b0c9f5388a7ab3aa222be2f4c60
Jared Wilkerson <wilkersonj13@gmail.com>
2011-05-26 14:58:25 -0400

    started work on problem three in c++

---
+++ ProblemThree/ProblemThree.cpp
@@ -0,0 +1,43 @@
+/***********
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ *
+ * *********/
+
+#include <vector>
+
+std::vector<int> primes;
+
+
+int getPrimes(int target){
+	int curr = 2;
+
+        while(curr < (.5 * x)){
+                prime = true;
+                for(int x=0;x<primes.length();++x){ 
+                        if( 0 == (curr%primes[x])){
+                                prime=false;
+                        }
+                } 
+		if(prime){
+			primes.push_back(curr);
+		}
+		++curr;
+        }
+}
+
+
+int main(){
+	bool found = false;
+	while(curr < (.5 * x)){
+        	prime = true;
+		for(int x=0;x<primes.length();++x){
+			if( 0 == (curr%primes[x])){
+				prime=false;
+			}
+		}   
+		
+	}
+}
