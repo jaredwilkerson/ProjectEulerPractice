96ecda2cbe0d3e96106924dd04d60b842fc77251
Jared Wilkerson <wilkerson.jared@gmail.com>
2012-01-26 11:18:31 -0500

    Successfull solution to Problem Three.
    Finishes in just under 5 seconds.

--- ProblemThree/ProblemThree.cpp
+++ ProblemThree/ProblemThree.cpp
@@ -8,60 +8,87 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
-std::vector<int> getPrimes(int target){
-	std::vector<int> primes;
-	
+struct c_unique {
+	long current;
+	c_unique() {current=2;}//start populating at the first prime, 2. otherwise it won't work
+	long operator()() {return ++current;}
+} UniqueNumber;
+
+std::vector<long> getPrimes(long target){
+	long tmpTarget = target;
+	int interval = 100000;
+	std::vector<long> primes;
+
 	if(1 < target){
-		int curr = 2;//start at the first prime
+		for(long x=0;x<tmpTarget; x=x+interval)//while the starting point is less than half the size of the temporary target
+		{
+			std::vector<long> nums(interval,0);//create a vector of longs that has a size of 10,000
+			generate (nums.begin(), nums.end(), UniqueNumber);//itinialize the vector with the current set of numbers
 
-		//half of the target is the largest possible prime number
-		while(curr <= (.5 * target)){
-			bool prime = true;//assume it is prime
-
-			//divide by the previous primes
-			for(int x=0;x<primes.size();++x){
-				//if evenly divisiable then not a prime	
-				if( 0 == (curr%primes[x])){
-					prime=false;
+			//loop through previous primes and set their multiples in the current data set to false
+			std::vector<long>::iterator primeItr, numItr;
+			for(primeItr=primes.begin(); primeItr!=primes.end();++primeItr){
+				for(numItr=nums.begin();numItr!=nums.end();){
+					if(0 == (*numItr % *primeItr)){
+						numItr = nums.erase(numItr);
+					}
+					else {
+						++numItr;
+					}
 				}
-			} 
-			//if prime, add to the list of primes
-			if(prime){
-				primes.push_back(curr);
 			}
-			++curr;//go to the next number
+
+			//start looping through current set for primes. first true found is prime and go from there
+			for(numItr=nums.begin();numItr!=nums.end();++numItr){
+				//std::cout << "I have " << primes.size() << " primes." << std::endl;
+				primes.push_back(*numItr);
+				while(0 == (tmpTarget%*numItr)){
+					tmpTarget /= *numItr;//use prime factorization to shrink the target
+				}
+				//std::cout << *numItr << " is prime." << std::endl;
+				std::vector<long>::iterator multipleItr;
+				for(multipleItr=numItr+1;multipleItr!=nums.end();){
+					//	std::cout << "Seeing if " << *multipleItr << " is a multiple of " << *numItr << std::endl;
+					if(0 == (*multipleItr % *numItr)){
+						//delete nums[x]
+						multipleItr = nums.erase(multipleItr);	
+					}
+					else{
+						++multipleItr;
+					}
+				}
+			}
 		}
 	}
-	else{
+	else{//The target number is too low
 		primes.push_back(0);
 		return primes;
-
 	}
 
 	return primes;
 }
 
-int getLargestFactor(std::vector<int> factors, int target){
-	int curr = 0;
-	int largest = 0;
+long getLargestFactor(std::vector<long> factors, long target){
+	long largest = 0;
 
-	while((.5 * target) > factors[curr]){
-		if((target % factors[curr]) == 0){
-			largest = factors[curr];
-			std::cout << "Current largest: " << factors[curr] << std::endl;
+	std::vector<long>::iterator itr;
+	for(itr = factors.begin(); itr != factors.end(); ++itr){
+		if((target % *itr) == 0){
+			largest = *itr;
 		}
-		++curr;
 	}
-
 	return largest;
 }
 
 
 int main(){
-
-	std::vector<int> primes = getPrimes(13195);
-	int largest = getLargestFactor(primes, 13195);
-	std::cout << "The largest prime is: " << primes[primes.size()-1] << std::endl;
+	long target = 0;
+	std::cout << "Input the target value: ";
+	std::cin >> target;
+	std::vector<long> primes = getPrimes(target);
+	long largest = getLargestFactor(primes, target);
+	//	std::cout << "The largest prime is: " << primes.back() << std::endl;
 	std::cout << "The largest factor is: " << largest << std::endl;
 }
